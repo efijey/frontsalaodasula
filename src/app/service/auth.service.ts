5e6044c8d5a8e01ea043632f897f09d484165cb8
ammac <alexandremmac@gmail.com>
2025-04-27 22:23:43 -0300

    changes

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,59 +1,32 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { AuthenticationRequest } from '../model/AuthenticationRequest';
-import { AuthenticationResponse } from '../model/AuthenticationResponse';
+import { Observable, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
-  public isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {
-    // Check if user is already logged in (from localStorage)
-    const token = localStorage.getItem('authToken');
-    if (token) {
-      this.isAuthenticatedSubject.next(true);
-    }
-  }
+  constructor(private http: HttpClient) {}
 
-  login(username: string, password: string): Observable<boolean> {
-    const request: AuthenticationRequest = { username, password };
-    
-    return new Observable(observer => {
-      this.http.post<AuthenticationResponse>(`${this.apiUrl}/login`, request).subscribe({
-        next: (response) => {
-          if (response.success && response.token) {
-            localStorage.setItem('authToken', response.token);
-            this.isAuthenticatedSubject.next(true);
-            observer.next(true);
-          } else {
-            observer.next(false);
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/auth/login`, { username, password })
+      .pipe(
+        tap((response: any) => {
+          if (response.token) {
+            localStorage.setItem('token', response.token);
           }
-          observer.complete();
-        },
-        error: () => {
-          observer.next(false);
-          observer.complete();
-        }
-      });
-    });
+        })
+      );
   }
 
   logout(): void {
-    localStorage.removeItem('authToken');
-    this.isAuthenticatedSubject.next(false);
+    localStorage.removeItem('token');
   }
 
   isLoggedIn(): boolean {
-    return this.isAuthenticatedSubject.value;
-  }
-
-  getToken(): string | null {
-    return localStorage.getItem('authToken');
+    return !!localStorage.getItem('token');
   }
 } 
